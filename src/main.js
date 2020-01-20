9226b31b0fa8f7d1662ca15152fac46746c337c9
Daniel <daniel.jaramillo7@udea.edu.co>
2020-01-20 13:57:00 -0500

    Added Axios, VueMaterial and a new view
    The new view has the very basic UI to interact with the ML API

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { MdButton } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(MdButton)
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/MercadoAPI.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="about">
+    <h1>Here we test the Mercado Libre API!</h1>
+    <div class="api-container">
+      <md-button class="md-raised md-primary" @click="onClickSearch">Search!</md-button>
+      <div class="search-container">
+        <p id="api-search-results">{{searchResults}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'MercadoAPI',
+  data() {
+    return {
+      searchResults: 'No search results yet!',
+    }
+  },
+  methods: {
+    onClickSearch() {
+      // eslint-disable-next-line no-console
+      console.log('search!')
+    }
+  },
+  mounted() {
+    // eslint-disable-next-line no-console
+    console.log(axios)
+  },
+}
+</script>
+
+<style scoped>
+.api-container {
+  display: flex;
+  width: 100%;
+  padding: 20px;
+}
+md-button {
+  align-items: center;
+}
+.search-container {
+  background-color: #f2f2f2;
+  width: 100%;
+  height: 80px;
+  /* width: 200px;
+  height: 500px; */
+}
+</style>
